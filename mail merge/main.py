d81e1cb076d998d2ab86deb87e3a521981e38e94
Tech_Tebz <lesedidakile@gmail.com>
2024-11-07 15:29:16 +0200

    main.py

--- mail merge/main.py
+++ mail merge/main.py
@@ -1,8 +1,48 @@
-#TODO: Create a letter using starting_letter.docx 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+import os
+import docx2txt
+
+def create_directory():
+    directory_name = "ReadyToSend"
+    if not os.path.exists(directory_name):
+        try:
+            os.mkdir(directory_name)
+        except OSError as exception:
+            print(f"Failed to create directory as {exception}")
+
+create_directory()
+
+def get_names():
+    try:    
+        with open(r"C:\Users\lenovo\Documents\Backend-Daily\mail merge\Input\Names\inivited_names.txt", "r", errors = "ignore") as names:
+            lines = names.readlines()
+        guest_list = []
+        for i in lines:
+            j = i.replace("\n", "")
+            guest_list.append(j)
+        print(guest_list)
+        return guest_list
+    except Exception as e:
+        print(f"Failed to open file as {e}")
+        return []
+
+def main():
+    names = get_names()
+    create_directory()
+    try:
+        file_path = r"C:\Users\lenovo\Documents\Backend-Daily\mail merge\Input\letters\starting_letter.docx"
+        text = docx2txt.process(file_path)
+
+        for name in names:
+            personal_invite = text.replace("name", name)
+            print(personal_invite)
+
+            output_path = f"C:/Users/lenovo/Documents/Backend-Daily/mail merge/Input/letters/ReadyToSend/{name}_invitation.txt"
+            with open(output_path, "w", errors = "ignore") as file:
+                file.write(personal_invite)
+            print(f"Document saved for {name}")
+
+    except Exception as e:
+        print(f"Failed to open the file as {e}")
+
+if __name__ == "__main__":
+    main()
